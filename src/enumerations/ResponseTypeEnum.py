d363392180584beb6614781e6ec09d787a5596e9
AngusTang <angus.tang@dbschenker.com>
2025-05-15 15:43:56 +0800

    adapt pie chart style

---
+++ src/enumerations/ResponseTypeEnum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class ResponseDisplayType(Enum):
+    RESPONSE_PIE_CHART="response_pie_chart"
+    RESPONSE_LINE_CHART="response_line_chart"
+    RESPONSE_TABLE="response_table"
+    RESPONSE_BAR_CHART="response_bar_chart"
--- src/response_obj/data_render.py
+++ src/response_obj/data_render.py
@@ -1,5 +1,5 @@
 from typing import Any,List
-
+from src.enumerations.ResponseTypeEnum import ResponseDisplayType
 
 class DataRender:
     def __init__(self,sql:str,display_type:str,data:Any,columns:List[str]):
@@ -7,3 +7,39 @@ class DataRender:
         self.display_type = display_type
         self.data = data
         self.columns = columns
+        self.value_column = ""
+        self.name_column = ""
+
+    def process_data_by_display_type(self):
+        print(self.display_type == ResponseDisplayType.RESPONSE_TABLE.value)
+        print(self.display_type == ResponseDisplayType.RESPONSE_PIE_CHART.value)
+        if self.display_type == ResponseDisplayType.RESPONSE_TABLE.value:
+            return
+        elif self.display_type == ResponseDisplayType.RESPONSE_PIE_CHART.value:
+            self.process_pie_chart()
+        elif self.display_type == ResponseDisplayType.RESPONSE_LINE_CHART.value:
+            return
+
+    def transform_item_for_pie_chart(self,item):
+        return {
+            "name":item[self.name_column],
+            "value":item[self.value_column]
+        }
+
+    def process_pie_chart(self):
+        pie_chart_data = []
+        pie_chart_labels = []
+        if(len(self.data) and len(self.columns) >0):
+            self.value_column = ""
+            self.name_column = ""
+            if isinstance(self.data[0][self.columns[0]],str):
+                self.name_column = self.columns[0]
+                self.value_column = self.columns[1]
+            else:
+                self.name_column = self.columns[1]
+                self.value_column = self.columns[0]
+            pie_chart_data = list(map(self.transform_item_for_pie_chart,self.data))
+            pie_chart_labels = [item[self.name_column] for item in self.data]
+        print(pie_chart_data)
+        self.data = pie_chart_data
+        self.columns = pie_chart_labels
