a1d6b1f2dbd42cae2f5f4eeb76568d4f5d5ca1ca
AngusTang <angus.tang@dbschenker.com>
2025-05-15 18:30:27 +0800

    add line chart

--- src/response_obj/data_render.py
+++ src/response_obj/data_render.py
@@ -18,6 +18,7 @@ class DataRender:
         elif self.display_type == ResponseDisplayType.RESPONSE_PIE_CHART.value:
             self.process_pie_chart()
         elif self.display_type == ResponseDisplayType.RESPONSE_LINE_CHART.value:
+            self.process_line_chart()
             return
 
     def transform_item_for_pie_chart(self,item):
@@ -43,3 +44,20 @@ class DataRender:
         print(pie_chart_data)
         self.data = pie_chart_data
         self.columns = pie_chart_labels
+
+    def process_line_chart(self):
+        line_chart_x = []
+        line_chart_y = []
+        if(len(self.data) and len(self.columns) >0):
+            self.value_column = ""
+            self.name_column = ""
+            if isinstance(self.data[0][self.columns[0]],str):
+                self.name_column = self.columns[0]
+                self.value_column = self.columns[1]
+            else:
+                self.name_column = self.columns[1]
+                self.value_column = self.columns[0]
+            line_chart_y = [item[self.value_column] for item in self.data]
+            line_chart_x = [item[self.name_column] for item in self.data]
+        self.data = line_chart_y
+        self.columns = line_chart_x
